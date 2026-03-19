a7f4be9fd4b4d4ba949fd28b774ee05b2643cb2c
Dessmin <quanglong281381@gmail.com>
2026-03-19 16:05:31 +0700

    feat: migration

---
+++ TripTogether.Domain/Migrations/20260319090516_PollOption-IsSelectFinalized.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TripTogether.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class PollOptionIsSelectFinalized : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
