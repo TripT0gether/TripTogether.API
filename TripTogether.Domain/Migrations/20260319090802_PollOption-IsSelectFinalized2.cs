78658648207a39f47014e02638fd7d7143ecee08
Dessmin <quanglong281381@gmail.com>
2026-03-19 16:08:14 +0700

    feat: migration

---
+++ TripTogether.Domain/Migrations/20260319090802_PollOption-IsSelectFinalized2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TripTogether.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class PollOptionIsSelectFinalized2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_select_finalized",
+                table: "poll_options",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_select_finalized",
+                table: "poll_options");
+        }
+    }
+}
